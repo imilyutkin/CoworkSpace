5b3f20781356e76946acc5b2cf6dcc396181ccbf
Ivan, Milyutkin <ivan.milyutkin@gmail.com>
2015-07-01 11:29:43 +0300

    added full version of default project

---
+++ CoworkSpace.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoworkSpace.Web.MvcApplication" Language="C#" %>
---
+++ CoworkSpace.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoworkSpace.Web.Startup))]
+namespace CoworkSpace.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
